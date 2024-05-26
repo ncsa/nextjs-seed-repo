bb436473ca9d1cc38cbd8bcb8799ba5688d0c825
Varsha C L <clvarsha1998@gmail.com>
2024-05-26 14:32:51 -0500

    fix: lint issues

--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,10 +1,10 @@
-import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-import Home from '../page'
- 
-describe('Home', () => {
-  it('renders home page correctly', () => {
-    const { container } = render(<Home />)
-    expect(container).toMatchSnapshot()
-  })
-})
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Home from "../page";
+
+describe("Home", () => {
+  it("renders home page correctly", () => {
+    const { container } = render(<Home />);
+    expect(container).toMatchSnapshot();
+  });
+});
